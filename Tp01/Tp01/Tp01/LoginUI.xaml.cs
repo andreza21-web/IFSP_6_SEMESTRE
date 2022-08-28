2bf350b0c14eae884bab93c084078963c74508e3
Andreza Pessoa <andreza.21pessoa@gmail.com>
2022-08-28 15:42:32 -0300

    tp01

---
+++ Tp01/Tp01/Tp01/LoginUI.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tp01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginUI : ContentPage
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Ok(object sender, EventArgs e)
+        {
+            if(txtUserName.Text == "admin"&& txtPassword.Text == "@admin"){
+                DisplayAlert("Autorizado", "Login Autorizado", "ok");
+            }
+            else
+            {
+                DisplayAlert("Usuario ou senha incorreto", "Login não autorizado", "ok");
+            }
+        }
+
+        private void Button_Limpar(object sender, EventArgs e)
+        {
+            txtUserName.Text = "";
+            txtPassword.Text = "";
+        }
+        private void Button_Creditos(object sender, EventArgs e)
+        {
+            DisplayAlert("Criado por:", "Andreza e Lidiane", "ok");
+        }
+
+    }
+}
